6cc156367ab61d0b79dc0bf64dd4006e406caff1
mianuar <53685685+imananuar@users.noreply.github.com>
2022-03-04 15:03:30 +0800

    adding onSubmit

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Create() {
+  
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState("mario");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+
+    console.log(blog);
+  }
+
   return (
-    <div>
+    <div className='create'>
         <h1>Add a new Blogs</h1>
+        <form onSubmit={handleSubmit}>
+          <label>Blog title:</label>
+          <input
+            type="text"
+            required
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <label>Blog body:</label>
+          <textarea
+            required
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+          ></textarea>
+          <label>Blog Author:</label>
+          <select
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+          >
+              <option value="mario">Mario</option>
+              <option value="yoshi">Yoshi</option>
+          </select>
+          <button>Add Blog</button>
+          <p> {title} </p>
+          <p> {body} </p>
+          <p> {author} </p>
+        </form>
     </div>
   )
 }
