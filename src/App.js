41a91a2ecb7f68511014c48706e26f910355a71e
mianuar <53685685+imananuar@users.noreply.github.com>
2022-03-04 10:58:15 +0800

    Adding route to New Blogs

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './components/Create';
 
 
 function App() {
@@ -11,8 +12,11 @@ function App() {
         <Navbar />
         <div className='content'>
           <Switch>
-              <Route path="/">
+              <Route exact path="/">
                 <Home />
+              </Route>
+              <Route path="/create">
+                <Create />
               </Route>
           </Switch>
         </div>
